b64ca0cc56e858106860cd2aeb83dc1c014d3175
Piotr Migdal <pmigdal@gmail.com>
2013-03-03 19:15:42 +0100

    flourish order fix

--- curva-turn.js
+++ curva-turn.js
@@ -11,11 +11,11 @@ function mod(n, m){
 	return (n % m + m) % m;
 }
 
-function World(){
+function World(k){
 	this.nodes = [];
 
 	this.init = function(k){
-		this.nodes.push({neighbours: span(1,k)});
+		this.nodes.push({neighbours: span(1,k), flourished: true});
 		for(var i = 0; i < k; i++){
 			this.bud(0, i);
 		}
@@ -27,29 +27,41 @@ function World(){
 		if(! this.nodes[children[i]]){
 			var node_on_left = children[mod(i + 1, children.length)];  // but "mod vs %" used only once, for init...
 			var node_on_right = children[mod(i - 1, children.length)];   // we look outwards; counterclockwise conv
-			this.nodes[children[i]] = {neighbours: [node_on_left, n, node_on_right]};
+			this.nodes[children[i]] = {neighbours: [node_on_left, n, node_on_right], flourished: false};
 			return true;
 		} else {
 			return false;
 		}
 	};
 
+	this.init(k);
+
 	this.flourish = function(n, k){
 		var node = this.nodes[n];
 		var k0 = node.neighbours.length;
 		var no_of_nodes = this.nodes.length;
-		if(k0 > 2){
-			this.nodes[node.neighbours[0]].neighbours.push(no_of_nodes);
-			this.nodes[node.neighbours[2]].neighbours.unshift(no_of_nodes + k - k0 - 1);
+		if(!node.flourished){
+			this.nodes[node.neighbours[0]].neighbours.push(no_of_nodes + k - k0 - 1);
+			this.nodes[node.neighbours[2]].neighbours.unshift(no_of_nodes);
 			var new_nodes = span(no_of_nodes, no_of_nodes + k - k0 - 1);
 			node.neighbours = node.neighbours.concat(new_nodes);
 			for(var i = 3; i < k; i++){
 				this.bud(n, i);
 			}
+			node.flourished = true;
 			return true;
 		} else {
 			return false;
 		}
 	};
+
+	this.toNeighbourList = function(){
+		var res = [];
+		for(var i = 0; i < this.nodes.length; i++){
+			res.push(this.nodes[i].neighbours);
+		}
+		return res;
+	};
+
 }
 
